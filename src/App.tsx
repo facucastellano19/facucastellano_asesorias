605d5a1ebdbefccfcb7dee79e2f41ee0b4bfb20f
Facundo Castellano <castellanofacundo05@gmail.com>
2026-03-13 21:59:16 -0300

    feat: add Problems section and refactor Services section with new Card component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 
 import Navbar from "./components/layout/Navbar";
 import Hero from "./components/sections/Hero";
+import Problems from "./components/sections/Problems";
 import Services from "./components/sections/Services";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <main>
         <Hero />
+        <Problems />
         <Services />
       </main>
     </div>
