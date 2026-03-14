56b62cc950bfc826585ead75c68ec56043272662
Facundo Castellano <castellanofacundo05@gmail.com>
2026-03-13 23:47:02 -0300

    feat(footer): add footer component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 
 import Navbar from "./components/layout/Navbar";
 import FAQ from "./components/sections/FAQ";
+import Footer from "./components/sections/Footer";
 import Hero from "./components/sections/Hero";
 import Problems from "./components/sections/Problems";
 import Services from "./components/sections/Services";
@@ -15,6 +16,7 @@ function App() {
         <Problems />
         <Services />
         <FAQ />
+        <Footer />
       </main>
     </div>
   );
